e8dd5c6b8421b2fc2e19444c386ae36a3f1be446
ZhangYuchen <1767230250@qq.com>
2023-09-22 16:52:14 +0800

    添加练习项目2，3，4

--- src/main/java/org/bw/flink1/Job4.java
+++ src/main/java/org/bw/flink1/Job4.java
@@ -1,7 +1,6 @@
 package org.bw.flink1;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -14,34 +13,39 @@ import org.apache.flink.util.Collector;
 * 1.单词是源源不断的输入的，可以基于nc指令来模拟这一场景
 *   nc -lk 9999 绑定9999端口持续输出
 * 2.基于对象来封装数据
+* 3.将业务逻辑代码也抽出来
+* 4.将job发布到flink远程平台
 *
 * */
-public class Job2 {
+public class Job4 {
     public static void main(String[] args) throws Exception {
         //1.创建程序入口
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //2.添加要处理的数据源
         DataStreamSource<String> source = env.socketTextStream("192.168.116.130", 9999);
         //3.处理数据
-        SingleOutputStreamOperator<WordCount> result = source.flatMap(new FlatMapFunction<String, WordCount>() {
-            @Override
-            public void flatMap(String s, Collector<WordCount> collector) throws Exception {
-                String[] words = s.split(",");
-                //Write
-                for (String word : words) {
-                    collector.collect(new WordCount(word, 1));
-                }
-            }
-        }).keyBy(t -> t.getWord()).sum("count");
+        SingleOutputStreamOperator<WordCount> result = source.flatMap(new Job3WordCount()).keyBy(t -> t.getWord()).sum("count");
 
         //4.输出结果
         result.print();
 
         //5.启动程序
-        env.execute("job2");
+        env.execute("job4");
 
 
     }
+
+    public static class Job3WordCount implements FlatMapFunction<String, WordCount> {
+        @Override
+        public void flatMap(String value, Collector<WordCount> out) throws Exception {
+            String[] words = value.split(",");
+            //Write
+            for (String word : words) {
+                out.collect(new WordCount(word, 1));
+            }
+        }
+    }
+
 
     public static class WordCount {
         private String word;
